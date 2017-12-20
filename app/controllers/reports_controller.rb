3a3cf38c55fb2f41822bb78b4944be47b1e847e5
Noah Schutte <noahschutte@gmail.com>
2017-12-19 18:12:23 -0700

    create csv

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,31 @@
 class ReportsController < ApplicationController
 
   def index
-    render :json => {
-      message: 'incomplete reports index route'
-    }
+    CSV.open("report.csv", "w") do |csv|
+      headers = [
+        'Date',
+        'Supplier Name',
+        'Invoice Number',
+        'Code',
+        'Amount',
+      ]
+      csv << headers
+
+      Item.all.each do |item|
+        invoice = Invoice.find(item.invoice_id)
+        row = [
+          invoice.date,
+          invoice.supplier_name,
+          invoice.invoice_number,
+          item.code,
+          item.amount
+        ]
+        csv << row
+      end
+      render :json => {
+        message: 'incomplete reports index route'
+      }
+    end
   end
 
 end
