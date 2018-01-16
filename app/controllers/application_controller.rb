8889b331fdb9887d7d468386e6a70ab7d9e2b2d7
Noah Schutte <noahschutte@gmail.com>
2018-01-16 16:41:27 -0700

    add cors preflight check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
+  before_filter :cors_preflight_check
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
